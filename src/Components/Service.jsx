374229e9138bed0d526d3b4832a52e92867ce231
Earthpasserby <iwuno68@gmail.com>
2025-06-03 13:35:08 +0100

    imageslide

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -3,7 +3,7 @@ const Service = () => {
     <>
       <div className="mx-auto w-full px-4 sm:px-14 lg:px-14 pt-4 sm:pt-24  bg-[#fbfbfb]">
         <div className="flex flex-col items-center justify-center mb-8">
-          <h3 className="font-bold  text-[#989898] text-2xl sm:text-[32px]">
+          <h3 className="font-bold  text-[#989898] md:text-2xl xl:text-2xl sm:text-xl">
             Keep Your Hands Clean and Ours Dirty
           </h3>
           <p className="text-[#988998] font-medium pt-2 max-w-xl text-center">
